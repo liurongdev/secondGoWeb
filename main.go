530b2ac385be643df81406d0564249379f10d25f
liurong <1069919773@qq.com>
2025-03-19 14:14:11 +0800

    grpc工具简化实现

--- main.go
+++ main.go
@@ -8,16 +8,15 @@ import (
 	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
-	name := flag.String("name", "wang", "用户名称")
-	fmt.Println(name)
-	flag.Parse()
-	global.InitViper("settings.dev.yml", "./config")
-	global.InitMysql(global.GetMysqlConfig())
-	redis.InitRedis(global.GetRedisConfig())
-	logger.Init()
+	start()
+	//testChan()
+}
+
+func start() {
 	// 初始化 Gin 路由引擎（默认包含 Logger 和 Recovery 中间件）
 	r := gin.Default()
 	user.Registry(r)
@@ -25,4 +24,38 @@ func main() {
 	// 启动服务器（默认监听 0.0.0.0:8080
 	port := global.Viper.GetString("settings.application.port")
 	r.Run(fmt.Sprintf(":%s", port))
+}
+
+func init() {
+	name := flag.String("name", "wang", "用户名称")
+	fmt.Println(name)
+	flag.Parse()
+	global.InitViper("settings.dev.yml", "./config")
+	global.InitMysql(global.GetMysqlConfig())
+	redis.InitRedis(global.GetRedisConfig())
+	logger.Init()
+}
+
+func testChan() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch1 <- "Message from ch1"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- "Message from ch2"
+	}()
+
+	select {
+	case msg1 := <-ch1:
+		fmt.Println("Received1:", msg1)
+	case msg2 := <-ch2:
+		fmt.Println("Received2:", msg2)
+	default:
+		fmt.Println("No reveived:")
+		time.Sleep(500 * time.Microsecond)
+	}
+
 }
